c77c9467977952643eb6b4f9690cc0827b199370
Eun-Young-lee <poply@hanmail.net>
2022-05-01 08:06:02 +0000

    commit

--- index.js
+++ index.js
@@ -48,11 +48,12 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
-const mongoose =require('mongoose');
+const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://poply:000@cluster0.okhyz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
 useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
-}). then(()=> console.log('MongoDB Connected...')).catch(err =>console.log(err));
+}). then(()=> console.log('MongoDB Connected...')).catch(err =>console.log(err))
 
+app.get('/', (req,res) => res.send('Hello World'));
 
 
 let map;
